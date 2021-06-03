28719ce326181af8a63a00e3dbd8bc5bf4fa440c
unknown <shubham75661@gmail.com>
2021-06-03 12:50:22 +0530

    Setup for Heroku

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const forecast = require("./utils/forecast")
 const { request } = require('http')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 const publicdirpath = path.join(__dirname,'../public')
 const viewpath = path.join(__dirname,'../templates/views')
@@ -78,6 +79,6 @@ app.get('*',(req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('server is up on port 3000')
+app.listen(port, () => {
+    console.log('server is up on port ' +port)
 })
